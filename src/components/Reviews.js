b2487859c51d5deae08b0f52f98b82ea1fcd723c
LJKT1 <94111910+LJKT1@users.noreply.github.com>
2022-02-08 15:02:32 +0000

    Add styling to review cards

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
 import { getReviews } from "../utils/api";
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
 
 const Reviews = (props) => {
   const [reviews, setReviews] = useState([]);
@@ -12,19 +15,31 @@ const Reviews = (props) => {
   return (
     <main className="Reviews">
       <h2>Reviews</h2>
+
       <ul>
-        {reviews.map((review) => {
-          return (
-            <li key={review.review_id}>
-              <p>{review.title}</p>
-              <p>Category: {review.category}</p>
-              <img src={review.review_img_url} alt={review.title} />
-              <p>By: {review.owner}</p>
-              <p>Votes: {review.votes}</p>
-              <p>Comments: {review.comment_count}</p>
-            </li>
-          );
-        })}
+        <Container fluid="md">
+          <Card style={{ width: "18rem" }}>
+            <ListGroup>
+              {reviews.map((review) => {
+                return (
+                  <ListGroup.Item key={review.review_id}>
+                    <Card.Title>{review.title}</Card.Title>
+                    <Card.Subtitle>{review.category}</Card.Subtitle>
+                    <Card.Img
+                      variant="top"
+                      src={review.review_img_url}
+                      alt={review.title}
+                    />
+                    <Card.Text>By: {review.owner} </Card.Text>
+                    <Card.Text>
+                      Votes: {review.votes} Comments: {review.comment_count}
+                    </Card.Text>
+                  </ListGroup.Item>
+                );
+              })}
+            </ListGroup>{" "}
+          </Card>
+        </Container>
       </ul>
     </main>
   );
